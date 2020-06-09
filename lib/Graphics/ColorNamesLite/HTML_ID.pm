0076dd7b05ddc6b01242f950b1c09f9a205a3c23
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-09 16:25:48 +0700

    Initial commit

---
+++ lib/Graphics/ColorNamesLite/HTML_ID.pm
@@ -0,0 +1,15 @@
+package Graphics::ColorNamesLite::HTML_ID;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# CODE: use Graphics::ColorNames::HTML_ID; use Data::Dump; my $t = Graphics::ColorNames::HTML_ID::NamesRgbTable(); $t->{$_} = sprintf "%06x", $t->{$_} for keys %$t; print 'our $NAMES_RGB_TABLE = '; dd $t;
+
+1;
+# ABSTRACT: HTML color names and equivalent RGB values (Indonesian translation) (lite version)
+
+=head1 SEE ALSO
+
+L<Graphics::ColorNames::HTML_ID>
